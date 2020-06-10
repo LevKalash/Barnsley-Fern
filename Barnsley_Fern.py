5d21274c6997c1744fbd88e23b716d15d30973f7
levak <leva.kalashnikov@gmail.com>
2020-06-10 16:15:22 +0300

    Commented, refactored (optimisated)

--- Barnsley_Fern.py
+++ Barnsley_Fern.py
@@ -1,62 +1,77 @@
-import sys, pygame, random
+import pygame
+import random
+import sys
+
 from pygame.locals import *
+# importing libraries
 
-pygame.init()
+pygame.init()  # Standart pygame initialisation
 
-WIDTH  = 400
-HEIGHT = 600
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIDTH = 600
+HEIGHT = 800  # setting window size
+detalisation = 30000  # setting detalisation of image (number of dots)
 
-pygame.display.set_caption('Barnsley\'s Fern')
+win = pygame.display.set_mode((WIDTH, HEIGHT), FULLSCREEN)  # Making window
 
-####------Colours------####
-DARKGREEN  = (  0,  64,   0)
-LIGHTGREEN = (  0, 166,  17)
-####-------------------####
+BGCOLOR = (0, 0, 0)
+FERNCOLOR = (0, 166, 17)  # setting colors
+
 
 def convert_point(tup):
-    tup    = list(tup)
-    tup[0] = (tup[0] + 2.5)  * ((WIDTH  - 20)/ 5)
+    tup = list(tup)
+    tup[0] = (tup[0] + 2.5) * ((WIDTH - 20)/5)
     tup[1] = HEIGHT - tup[1] * ((HEIGHT - 20)/10) - 10
-
     return tuple(tup)
+
 
 def f1(point):
     x = 0
     y = 0.16 * point[1]
-
     return x, y
+
 
 def f2(point):
-    x =  0.85 * point[0] + 0.04 * point[1]
+    x = 0.85 * point[0] + 0.04 * point[1]
     y = -0.04 * point[0] + 0.85 * point[1] + 1.6
-
     return x, y
+
 
 def f3(point):
-    x =  0.20 * point[0] - 0.26 * point[1]
-    y =  0.23 * point[0] + 0.22 * point[1] + 1.6
-
+    x = 0.20 * point[0] - 0.26 * point[1]
+    y = 0.23 * point[0] + 0.22 * point[1] + 1.6
     return x, y
+
 
 def f4(point):
     x = -0.15 * point[0] + 0.28 * point[1]
-    y =  0.26 * point[0] + 0.24 * point[1] + 0.44
-
+    y = 0.26 * point[0] + 0.24 * point[1] + 0.44
     return x, y
+
 
 def main():
     point = (0, 0)
-    SCREEN.fill(DARKGREEN)
-
-    while True:
-        SCREEN.fill(LIGHTGREEN, (convert_point(point), (1, 1)))
-
+    win.fill(BGCOLOR)
+    for i in range(detalisation + 1):
+        win.fill(FERNCOLOR, (convert_point(point), (1, 1)))
         rand = random.random()
+
+        pygame.draw.rect(win, BGCOLOR, (0, 0, 300, 30))
+        win.blit(pygame.font.Font(None, 20).render(f"{i}/{detalisation}", 0, (255, 255, 255)), (5, 5))
+
+        if rand <= 0.01:
+            point = f1(point)
+        elif rand <= 0.86:
+            point = f2(point)
+        elif rand <= 0.93:
+            point = f3(point)
+        else:
+            point = f4(point)
+        """   
         if   rand <= 0.01: point = f1(point)
         elif rand >  0.01 and rand <= 0.86: point = f2(point)
         elif rand >  0.86 and rand <= 0.93: point = f3(point)
         elif rand >  0.95 and rand <= 1.00: point = f4(point)
+        """
 
         events = pygame.event.get()
         for event in events:
@@ -64,11 +79,21 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
+                if event.key == K_q:
                     pygame.quit()
                     sys.exit()
-
         pygame.display.update()
+
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_q:
+                    pygame.quit()
+                    sys.exit()
 
 if __name__ == '__main__':
     main()
